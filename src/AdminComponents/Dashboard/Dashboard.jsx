ba9fb9ff30872f3e6d9d9c349369175a24e6e8aa
Nafiz-Al-Turabi <nafizalturabi18cse@gmail.com>
2024-01-11 14:26:14 +0600

    Admin Dashboard UI Added

--- src/AdminComponents/Dashboard/Dashboard.jsx
+++ src/AdminComponents/Dashboard/Dashboard.jsx
@@ -2,8 +2,26 @@ import React from 'react';
 
 const Dashboard = () => {
     return (
-        <div>
-            this is Dashboard
+        <div className='admin grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 text-white'>
+            <div className='text-center bg-rose-400 h-56 flex justify-center items-center rounded-lg '>
+                <div>
+                    <h1 className='text-xl font-extrabold'>Total Student</h1>
+                    <p className='text-8xl font-extrabold'>655</p>
+                </div>
+            </div>
+            <div className='text-center bg-cyan-400 h-56 flex justify-center items-center rounded-lg'>
+                <div>
+                    <h1 className='text-xl font-extrabold'>Total Teacher</h1>
+                    <p className='text-8xl font-extrabold'>45</p>
+                </div>
+            </div>
+            <div className='text-center bg-yellow-400 h-56 flex justify-center items-center rounded-lg'>
+                <div>
+                    <h1 className='text-xl font-extrabold'>Total Committee Member</h1>
+                    <p className='text-8xl font-extrabold'>20</p>
+                </div>
+            </div>
+
         </div>
     );
 };
