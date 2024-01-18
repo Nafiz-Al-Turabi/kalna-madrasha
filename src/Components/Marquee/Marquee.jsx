1ba420b451246b659e6973cb60881946952c7dfb
Nafiz-Al-Turabi <nafizalturabi18cse@gmail.com>
2024-01-19 02:20:54 +0600

    marquee updated

--- src/Components/Marquee/Marquee.jsx
+++ src/Components/Marquee/Marquee.jsx
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Marquee from 'react-fast-marquee';
+import { FaGg } from 'react-icons/fa';
+import axiosInstance from '../../Global/Axios/AxiosInstance';
 
 const Headline = () => {
+    const [headline, setHeadline] = useState([]);
+
+    useEffect(() => {
+        fetchDataHeadline();
+    }, []);
+
+    const fetchDataHeadline = async () => {
+        try {
+            const response = await axiosInstance.get('/headline');
+            setHeadline(response.data);
+            console.log(response.data);
+        } catch (error) {
+            console.error('failed to fetch Headline', error);
+        }
+    };
+
     return (
-        <div className='py-5'>
-            <Marquee>
-                <p>মাধ্যমিক বিদ্যালয়ে ভর্তি: অনলাইনে আবেদন শুরু ২৪ অক্টোবর, পছন্দক্রম ৫টি বিদ্যালয়</p>
-                <p>মাধ্যমিক বিদ্যালয়ে ভর্তি: অনলাইনে আবেদন শুরু ২৪ অক্টোবর, পছন্দক্রম ৫টি বিদ্যালয়</p>
-                <p>মাধ্যমিক বিদ্যালয়ে ভর্তি: অনলাইনে আবেদন শুরু ২৪ অক্টোবর, পছন্দক্রম ৫টি বিদ্যালয়</p>
-                <p>মাধ্যমিক বিদ্যালয়ে ভর্তি: অনলাইনে আবেদন শুরু ২৪ অক্টোবর, পছন্দক্রম ৫টি বিদ্যালয়</p>
-                <p>মাধ্যমিক বিদ্যালয়ে ভর্তি: অনলাইনে আবেদন শুরু ২৪ অক্টোবর, পছন্দক্রম ৫টি বিদ্যালয়</p>
+        <div className='py-2'>
+            <Marquee speed={100}>
+                {headline.map((single, index) => (
+                    <p key={index} className='flex items-center'>
+                        <FaGg className='text-3xl mx-5 text-[#DAA520]' /> {single.headline}
+                    </p>
+                ))}
             </Marquee>
         </div>
     );
